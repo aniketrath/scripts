df6a70b6394f7aaa10a1a86a16c44f2df9f4bc2b
Aniket Rath <aniketrath1121@gmail.com>
2025-02-16 20:46:52 +0530

    Update Debian.tf

--- Debian.tf
+++ Debian.tf
@@ -10,10 +10,10 @@ terraform {
 provider "docker" {}
 # Build the Docker image from the Dockerfile
 resource "docker_image" "jenkins_agent" {
-  name = "jenkins-agent:latest"
+  name         = "jenkins-agent:latest"
+  keep_locally = false
   build {
-    context      = "./Docker"                   # The directory containing the Dockerfile
-    dockerfile   = "./Docker/Debian.Dockerfile" # Path to the Dockerfile
-    keep_locally = false
+    context    = "./Docker"                   # The directory containing the Dockerfile
+    dockerfile = "./Docker/Debian.Dockerfile" # Path to the Dockerfile
   }
 }
